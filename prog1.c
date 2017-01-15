3ada1cd033ab1ffc7b401ea0bc8a24a55476fd82
wil <wilmah@uniserve.com>
2017-01-14 22:33:40 -0800

    Some more changes

--- prog1.c
+++ prog1.c
@@ -1,6 +1,10 @@
 // This is my first file before it is intialized as a git project.
+#define MY_NUMBER	1000
 void prog1(void)
 {
-	printf("My prog1\n");
+	printf("My %d prog1\n", MY_NUMBER);
+
+	// Here's a change introduced new on alternate branch
+	printf("My alternate change\n");
 }
 
